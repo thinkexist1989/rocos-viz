7f7f458f75e44236ece5c5255645fbc88bf10ef5
Yang Luo <992534544@qq.com>
2021-09-09 13:43:35 +0800

    change style to Fusion

--- rocos_zh_CN.ts
+++ rocos_zh_CN.ts
@@ -5,6 +5,8 @@
     <name>CartesianPositionWidget</name>
     <message>
         <location filename="src/CartesianPositionWidget.ui" line="23"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_CartesianPositionWidget.h" line="88"/>
+        <location filename="build/rocos_autogen/include_Release/ui_CartesianPositionWidget.h" line="88"/>
         <source>Form</source>
         <translation type="unfinished"></translation>
     </message>
@@ -18,31 +20,43 @@
     <name>ConnectDialog</name>
     <message>
         <location filename="src/ConnectDialog.ui" line="22"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_ConnectDialog.h" line="83"/>
+        <location filename="build/rocos_autogen/include_Release/ui_ConnectDialog.h" line="83"/>
         <source>Dialog</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/ConnectDialog.ui" line="28"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_ConnectDialog.h" line="84"/>
+        <location filename="build/rocos_autogen/include_Release/ui_ConnectDialog.h" line="84"/>
         <source>IP Address:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/ConnectDialog.ui" line="35"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_ConnectDialog.h" line="85"/>
+        <location filename="build/rocos_autogen/include_Release/ui_ConnectDialog.h" line="85"/>
         <source>6666</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/ConnectDialog.ui" line="42"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_ConnectDialog.h" line="86"/>
+        <location filename="build/rocos_autogen/include_Release/ui_ConnectDialog.h" line="86"/>
         <source>192.168.0.99</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/ConnectDialog.ui" line="49"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_ConnectDialog.h" line="87"/>
+        <location filename="build/rocos_autogen/include_Release/ui_ConnectDialog.h" line="87"/>
         <source>Port:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/ConnectDialog.ui" line="62"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_ConnectDialog.h" line="88"/>
+        <location filename="build/rocos_autogen/include_Release/ui_ConnectDialog.h" line="88"/>
         <source>Connect</source>
         <translation type="unfinished"></translation>
     </message>
@@ -51,6 +65,8 @@
     <name>JointPositionWidget</name>
     <message>
         <location filename="src/JointPositionWidget.ui" line="23"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_JointPositionWidget.h" line="85"/>
+        <location filename="build/rocos_autogen/include_Release/ui_JointPositionWidget.h" line="85"/>
         <source>Form</source>
         <translation type="unfinished"></translation>
     </message>
@@ -59,6 +75,14 @@
         <location filename="src/JointPositionWidget.ui" line="32"/>
         <location filename="src/JointPositionWidget.ui" line="71"/>
         <location filename="src/JointPositionWidget.ui" line="74"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_JointPositionWidget.h" line="87"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_JointPositionWidget.h" line="90"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_JointPositionWidget.h" line="94"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_JointPositionWidget.h" line="97"/>
+        <location filename="build/rocos_autogen/include_Release/ui_JointPositionWidget.h" line="87"/>
+        <location filename="build/rocos_autogen/include_Release/ui_JointPositionWidget.h" line="90"/>
+        <location filename="build/rocos_autogen/include_Release/ui_JointPositionWidget.h" line="94"/>
+        <location filename="build/rocos_autogen/include_Release/ui_JointPositionWidget.h" line="97"/>
         <source>Jogging</source>
         <translation type="unfinished"></translation>
     </message>
@@ -75,26 +99,36 @@
     <name>RocosMainWindow</name>
     <message>
         <location filename="src/RocosMainWindow.ui" line="23"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="404"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="404"/>
         <source>ROCOS-MainWindow</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="30"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="421"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="421"/>
         <source>3D Visualization</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="39"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="422"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="422"/>
         <source>Axes</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="46"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="423"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="423"/>
         <source>Mesh</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="53"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="424"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="424"/>
         <source>Plane</source>
         <translation type="unfinished"></translation>
     </message>
@@ -102,146 +136,208 @@
         <location filename="src/RocosMainWindow.ui" line="60"/>
         <location filename="src/RocosMainWindow.ui" line="371"/>
         <location filename="src/RocosMainWindow.ui" line="374"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="414"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="416"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="425"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="414"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="416"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="425"/>
         <source>Trajectory</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="70"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="426"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="426"/>
         <source>Cartesian Space</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="97"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="427"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="427"/>
         <source>Joint Space</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="127"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="428"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="428"/>
         <source>Working Mode</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="140"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="429"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="429"/>
         <source>Position Mode</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="145"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="430"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="430"/>
         <source>Velocity Mode</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="150"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="431"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="431"/>
         <source>Torque Mode</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="155"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="432"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="432"/>
         <source>Joint Impedance Mode</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="160"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="433"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="433"/>
         <source>Cartisian Impedance Mode</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="171"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="435"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="435"/>
         <source>Reference Frame</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="177"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="436"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="436"/>
         <source>World</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="184"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="437"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="437"/>
         <source>Base</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="191"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="438"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="438"/>
         <source>Flange</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="198"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="439"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="439"/>
         <source>Tool</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="208"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="440"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="440"/>
         <source>Precise Cartesian Control</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="226"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="441"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="441"/>
         <source>PushButton</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="234"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="442"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="442"/>
         <source>Precise Joint Control</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="241"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="443"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="443"/>
         <source>Speed Scaling</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="288"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="444"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="444"/>
         <source>toolBar</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="314"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="405"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="405"/>
         <source>frontView</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="317"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="407"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="407"/>
         <source>FrontView</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="326"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="409"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="409"/>
         <source>TopView</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="335"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="410"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="410"/>
         <source>RightView</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="344"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="411"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="411"/>
         <source>AxonometricView</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="353"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="412"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="412"/>
         <source>AxesView</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="362"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="413"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="413"/>
         <source>Plan</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="383"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="418"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="418"/>
         <source>Ground</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="399"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="419"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="419"/>
         <source>Enabled</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/RocosMainWindow.ui" line="409"/>
+        <location filename="build/rocos_autogen/include_Debug/ui_RocosMainWindow.h" line="420"/>
+        <location filename="build/rocos_autogen/include_Release/ui_RocosMainWindow.h" line="420"/>
         <source>Connected</source>
         <translation type="unfinished"></translation>
     </message>
--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,16 @@
 #include <QApplication>
 
 #include <QSurfaceFormat>
+#include <QStyleFactory>
+
 #include <QVTKOpenGLNativeWidget.h>
 
 int main(int argc, char *argv[])
 {
     // needed to ensure appropriate OpenGL context is created for VTK rendering.
     QSurfaceFormat::setDefaultFormat(QVTKOpenGLNativeWidget::defaultFormat());
+
+    QApplication::setStyle(QStyleFactory::create("Fusion"));
 
     QApplication a(argc, argv);
     a.setWindowIcon(QIcon(":/icon.png"));
